06e3fb4397e1a42d5ed0c021d81819b3df57023d
nnlmc <nnlmc@users.noreply.github.com>
2026-06-26 12:25:11 +0800

    fix: 初始化前创建配置目录

--- ChisaEating/chisaeating_config/__init__.py
+++ ChisaEating/chisaeating_config/__init__.py
@@ -3,4 +3,5 @@ from gsuid_core.utils.plugins_config.gs_config import StringConfig
 from .config_default import CONFIG_DEFAULT
 
 CONFIG_PATH = get_res_path() / "ChisaEating" / "config.json"
+CONFIG_PATH.parent.mkdir(parents=True, exist_ok=True)
 CHISA_CONFIG = StringConfig("ChisaEating", CONFIG_PATH, CONFIG_DEFAULT)
